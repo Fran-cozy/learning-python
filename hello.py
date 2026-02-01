e52b3dee1f7e0d6bc2fd54bcd0c998ca0d5cc25e
Fran-cozy <isiborfrancis180@gmail.com>
2026-02-01 05:17:05 -0800

    futher update

---
+++ hello.py
@@ -0,0 +1,131 @@
+
+# variable = value
+# totalMarks = 95
+# print(totalMarks)
+# totalMarks = 100
+# print(totalMarks)
+# integerNumber = 10
+# floatingNumber = 20.75 
+# print(floatingNumber)
+# print(integerNumber)
+# print(type(floatingNumber))
+# floatingNumber = int(floatingNumber)
+# print(type(floatingNumber))
+# print(floatingNumber)
+# 3 + 2
+# print(3+2)
+# 10 - 8
+# print(10-8)
+# 4 * 4
+# print(4*4)
+# 10 / 5
+# print(10/5)
+# 10 // 5
+# print(10//5)
+# 15 % 10
+# print(15%10)
+# 2 < 5
+# print(2<5)
+# 5 == 5
+# print(5==5)
+# 6 <= 5
+# print(6<=5)
+# 2 != 5
+# print(2!=5)
+# 2<5 and 5<10
+# print(2<5 and 5<10)
+# 2<5 and 5>10
+# print(2<5 and 5>10)
+# 5>10 and 2<5
+# print(5>10 and 2<5)
+# 2 > 5
+# print(2>5)
+# not 2>5
+# print(not 2>5)
+
+# # stringName = "string" or 'string'
+# fruit = "Apple"
+# print(fruit)
+# fruit[0]
+# print(fruit[0])
+# fruit[4]
+# print(fruit[4])
+
+# # string is one among python's data types
+# string = "string is one among python's data types"
+# print(string)
+# string = 'string is one among python\' data types'
+# print(string)
+# print("python is a great programing language for beginners")
+# programminglanguage = "python"
+# print(programminglanguage)
+# print("{} is a great programming language for beginners".format(programminglanguage))
+# string = "python programming is easy"
+# print(string)
+# string.upper()
+# print(string.upper())
+# string.lower()
+# print(string.lower())
+
+#  # replace
+# string.replace("easy", "powerful")
+# print(string.replace("easy", "powerful")) 
+
+#  # slice
+# string[0:6]
+# print(string[0:6])
+
+# # listname = [object2, object2, object 3]
+# favoritefruits = ["Apple", "Mango", "strawbberry"]
+# print(favoritefruits)
+# favoritefruits[0]
+# print(favoritefruits[0])
+# favoritefruits[1] = "orange"
+# print(favoritefruits[1])
+# print(favoritefruits)
+
+# # list.append
+# favoritefruits.append("kiwi")
+# print(favoritefruits)
+# favoritefruits.insert(1, "mango")
+# print(favoritefruits)
+
+# #  list.remove()
+# favoritefruits.remove("strawbberry")
+# print(favoritefruits)
+
+# # list.sort()
+# favoritefruits.sort()
+# print(favoritefruits)
+
+# # list.reverse()
+# favoritefruits.reverse()
+# print(favoritefruits)
+
+# # list.pop()
+# favoritefruits.pop()
+# print(favoritefruits)
+
+# # tupleName = (object1, object2, object3)
+# # tuple to store dates of world war I and world war II
+# historicWarDates = (1914, 1939)
+# historicWarDates[0]
+# print(historicWarDates[0])
+# historicWarDates[1]
+# print(historicWarDates[1])
+# del(historicWarDates)
+# print(historicWarDates)
+
+# dictionaryName = {key1: vaue, key2: value}
+# Dictionary to map the price of a camera to it brand
+priceOfCameras = {"sony": 500, "nikon": 600, "canon": 700}
+print(priceOfCameras)
+# priceOfCameras["sony"]
+print(priceOfCameras["sony"])
+
+# dictionary.keys()
+priceOfCameras.keys()
+print(priceOfCameras.keys())
+
+# dictionary.values()
+print(priceOfCameras.value)
